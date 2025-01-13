8888cd6cd3f5c7ac9a683b3fd293fff8d7161d18
dasilvacsv <dasilva.csv@gmail.com>
2025-01-13 11:19:47 -0400

    Refactoring a bit

--- src/app/(app)/hi/page.tsx
+++ src/app/(app)/hi/page.tsx
@@ -1,13 +1,9 @@
-import React from 'react'
-import Dashboard from './component'
-import { Component } from './comp'
-import { getDashboardData } from './fetch'
-
+import Component from "@/features/dashboard/Dashboard"
+import { getDashboardData } from "@/features/dashboard/fetch"
 
 
 export default async function Page() {
     const data = await getDashboardData()
-
 
     return (
         <div>
